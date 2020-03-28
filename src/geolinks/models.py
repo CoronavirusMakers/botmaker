6ac3cef823242b105a91222bec82e2fffc7159b7
Alberto Morales <amd77@galotecnia.com>
2020-03-28 12:54:30 +0000

    Unique para las urls

--- src/geolinks/models.py
+++ src/geolinks/models.py
@@ -64,7 +64,7 @@ class Place(models.Model):
 class Uri(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     description = models.TextField(blank=True, null=True)
     location = PlainLocationField(based_fields=['place'], zoom=7, blank=True, null=True)
 
