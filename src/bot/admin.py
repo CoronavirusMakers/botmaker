529c26d68ab06846ea24bd998a90c76bfe6b7e0b
Alberto Morales <amd77@galotecnia.com>
2020-03-29 21:44:30 +0100

    Mejorada admin

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -5,7 +5,7 @@ from .models import TelegramUser
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
     date_hierarchy = 'creation_date'
-    list_display = ('ident', 'username', 'first_name', 'last_name', 'creation_date', 'web_group')
-    readonly_fields = ('creation_date', )
+    list_display = ('ident', 'username', 'first_name', 'last_name', 'creation_date', 'web_group', 'receive_unknown')
+    readonly_fields = ('creation_date', 'ident', 'username', 'first_name', 'last_name', 'is_bot', 'language_code')
     list_filter = ('web_group', 'receive_unknown')
     search_fields = ('username', 'first_name', 'last_name')
